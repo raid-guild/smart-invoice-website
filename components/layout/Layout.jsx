0c43873c100e91d54eb755e6e06f1361e401e8b3
George <gbh218@gmail.com>
2022-09-06 13:26:51 -0600

    Add google analytics script

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Box, Flex } from '@chakra-ui/react';
 import Head from 'next/head';
+import Script from 'next/script';
 import React from 'react';
 
 import { Footer } from './Footer';
@@ -13,7 +14,23 @@ export function Layout({ children }) {
         <meta name="description" content="Smart Invoice is an easy-to-use tool that provides web3 freelancers with cryptocurrency invoicing, escrow, and arbitration." />
         <link rel="icon" href="/logos/smart-invoice/icon-blue.svg" />
         {/* <!-- Google tag (gtag.js) -->  */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-30565BWGW9"></script>
+        <Script
+          async
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-30565BWGW9`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-30565BWGW9');
+            `,
+          }}
+        />
       </Head>
 
       <NavBar />
