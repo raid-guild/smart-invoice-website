4c053a9d9a2d054c3d7e5dbad958f3ed7ef47718
George <gbh218@gmail.com>
2022-09-07 10:18:51 -0600

    Add and modify meta tags

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -10,16 +10,23 @@ export function Layout({ children }) {
   return (
     <Flex direction="column" minHeight="100vh">
       <Head>
-        <title>Smart Invoice</title>
-        <meta name="description" content="Smart Invoice is an easy-to-use tool that provides web3 freelancers with cryptocurrency invoicing, escrow, and arbitration." />
-        {/* <!-- Open Graph data --> */}
-        <meta property="og:title" content="Smart Invoice" />
-        <meta property="og:url" content="https://www.smartinvoice.xyz/" />
-        <meta property="og:image" content="/meta/home-graphic.png" />
-        <meta property="og:description" content="Smart Invoice is an easy-to-use tool that provides web3 freelancers with cryptocurrency invoicing, escrow, and arbitration." />
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Smart Invoice | Crypto Invoicing & Escrow Software</title>
+        <meta name="description" content="Free cryptocurrency invoicing and escrow software for freelancers. Securely accept payments in ETH, WETH, USDC, USDT, DAI, XDAI, WBTC, and many more tokens!"/>
+        <meta name="keywords" content="online invoicing software, cryptocurrency invoicing, free invoicing, invoices, escrow, crypto escrow, crypto payments"/>
+        <meta name="author" content="Smart Invoice"/>
+        <meta property="og:title" content="Crypto Invoicing & Escrow | Smart Invoice"/>
+        <meta property="og:description" content="Free cryptocurrency invoicing and escrow software for freelancers. Securely accept payments in ETH, WETH, USDC, USDT, DAI, XDAI, WBTC, and many more tokens!"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Smart Invoice"/>
+        <meta property="og:image" content="https://smartinvoice.xyz/meta/home-graphic.png"/>
+        <meta property="og:url" content="https://smartinvoice.xyz"/>
+        <meta name="twitter:card" content="https://smartinvoice.xyz/meta/about-graphic.png"/>
+        <meta name="twitter:image:alt" content="Crypto Invoicing & Escrow Tool"/>
+        <meta name="twitter:site" content="@SmartInvoiceXYZ"/>
         <link rel="icon" href="/logos/smart-invoice/icon-blue.svg" />
       </Head>
-      {/* <!-- Google tag (gtag.js) -->  */}
       <Script
         async
         strategy="afterInteractive"
